3d657f2fc0ece9f6fbb5cbcee5475daab313049c
Vikas Dharra <134390453+vikass01@users.noreply.github.com>
2023-12-11 21:52:12 +0530

    11 dec
    front page work

---
+++ src/utils/constants.js
@@ -0,0 +1,363 @@
+import {
+  FaHtml5,
+  FaCss3,
+  FaReact,
+  FaSass,
+  FaPython,
+  FaNodeJs,
+  FaDocker,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+
+import {
+  TbBrandTypescript,
+  TbBrandJavascript,
+  TbBrandCpp,
+  TbBrandRedux,
+  TbBrandBootstrap,
+} from "react-icons/tb";
+import { BiLogoJquery, BiLogoPostgresql } from "react-icons/bi";
+import {
+  SiPug,
+  SiExpress,
+  SiPostman,
+  SiGnubash,
+  SiNextdotjs,
+} from "react-icons/si";
+import { AiFillCode } from "react-icons/ai";
+import { FaHashnode } from "react-icons/fa6";
+import {
+  BsGlobe,
+  BsCodeSlash,
+  BsRobot,
+  BsFileBarGraph,
+  BsGit,
+  BsMarkdown,
+} from "react-icons/bs";
+import { GiArchiveResearch, GiArtificialIntelligence } from "react-icons/gi";
+import { FcElectronics } from "react-icons/fc";
+
+export const links = [
+  { text: "Home", path: "/" },
+  { text: "About", path: "/about" },
+  { text: "Skills", path: "/skills" },
+  { text: "Projects", path: "/projects" },
+  { text: "Experience", path: "/experience" },
+  { text: "Blogs", path: "/blogs" },
+  { text: "Contact", path: "/contact" },
+  { text: "Resume", path: "/resume" },
+];
+
+export const interestsData = [
+  {
+    interest: "Software Engineering",
+    icon: BsCodeSlash,
+  },
+  {
+    interest: "Tech Blog",
+    icon: FaHashnode,
+  },
+  {
+    interest: "Internet of Things",
+    icon: FcElectronics,
+  },
+  {
+    interest: "Market Research",
+    icon: GiArchiveResearch,
+  },
+  {
+    interest: "Visualization",
+    icon: BsFileBarGraph,
+  },
+  {
+    interest: "Software Development",
+    icon: AiFillCode,
+  },
+  {
+    interest: "Robotics",
+    icon: BsRobot,
+  },
+  {
+    interest: "Web Scraping",
+    icon: BsGlobe,
+  },
+];
+
+export const skillsData = [
+  {
+    name: "HTML5",
+    icon: FaHtml5,
+  },
+  {
+    name: "CSS3",
+    icon: FaCss3,
+  },
+  {
+    name: "Javascript",
+    icon: TbBrandJavascript,
+  },
+  {
+    name: "Jquery",
+    icon: BiLogoJquery,
+  },
+  {
+    name: "Typescript",
+    icon: TbBrandTypescript,
+  },
+  {
+    name: "SASS",
+    icon: FaSass,
+  },
+  {
+    name: "Bootstrap",
+    icon: TbBrandBootstrap,
+  },
+  {
+    name: "C/C++",
+    icon: TbBrandCpp,
+  },
+  {
+    name: "Python",
+    icon: FaPython,
+  },
+  {
+    name: "ReactJS",
+    icon: FaReact,
+  },
+  {
+    name: "NextJS",
+    icon: SiNextdotjs,
+  },
+  {
+    name: "Redux",
+    icon: TbBrandRedux,
+  },
+  {
+    name: "NodeJs",
+    icon: FaNodeJs,
+  },
+  {
+    name: "PugJs",
+    icon: SiPug,
+  },
+  {
+    name: "Express",
+    icon: SiExpress,
+  },
+  {
+    name: "Docker",
+    icon: FaDocker,
+  },
+  {
+    name: "Postgres",
+    icon: BiLogoPostgresql,
+  },
+  {
+    name: "Postman",
+    icon: SiPostman,
+  },
+  {
+    name: "Git",
+    icon: BsGit,
+  },
+  {
+    name: "Github",
+    icon: FaGithub,
+  },
+  {
+    name: "ChatGPT",
+    icon: GiArtificialIntelligence,
+  },
+  {
+    name: "Bash",
+    icon: SiGnubash,
+  },
+  {
+    name: "Markdown",
+    icon: BsMarkdown,
+  },
+];
+
+export const workData = [
+  {
+    company: "1Digitalstack.ai",
+    designation: "Software Engineer 1",
+    duration: "March 2022 - Present",
+    companyImg: "1ds.jpg",
+    description: (
+      <>
+        <ul>
+          <li>
+            I work on Reviniti, an analytics product designed for e-commerce
+            players in the bid management domain. Solely migrated the old
+            codebase from a templating engine (PugJS) to React with TypeScript.
+          </li>
+          <li>
+            Engaged in developing new features, refactoring code, enhancing
+            product performance, bug fixes and contributing towards achieving
+            high scalability for the product.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    company: "Fact.MR",
+    designation: "Associate Consultant Level 01",
+    duration: "Dec 2020 - July 2021",
+    companyImg: "fmr.svg",
+    description: (
+      <>
+        <ul>
+          <li>
+            Engaged in creating comprehensive client-specific and syndicated
+            research reports, along with SEO-focused articles and collateral
+            materials within the domains of chemicals, materials, consumer
+            goods, and industrial goods.
+          </li>
+          <li>
+            Expertly estimated market valuations and conducted primary research
+            in collaboration with industry experts to validate data, extract
+            valuable market insights.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+];
+
+export const projectsData = [
+  {
+    type: "WEB-APP",
+    title: "Movie Magnet",
+    image: "movie-magnet",
+    link: "https://movie-magnet-smoky.vercel.app/",
+    source: "https://github.com/vaheedsk36/movie-magnet",
+  },
+  {
+    type: "WEB-APP",
+    title: "Portfolio Website",
+    image: "portfolio-website",
+    link: null,
+    source: "https://github.com/vaheedsk36/portfolio-website-nextjs",
+  },
+  {
+    type: "WEB-APP",
+    title: "Weather App",
+    image: "weather-app",
+    link: "https://vaheedsk36.github.io/weather-app/",
+    source: "https://github.com/vaheedsk36/weather-app",
+  },
+  {
+    type: "WEB-APP",
+    title: "Landing Page",
+    image: "landing-page",
+    link: "https://vaheedsk36.github.io/demo-frontend-bootcamp",
+    source: "https://github.com/vaheedsk36/demo-frontend-bootcamp",
+  },
+  {
+    type: "WEB-APP",
+    title: "Guess Game",
+    image: "guess-game",
+    link: "https://vaheedsk36.github.io/guess-my-number",
+    source: "https://github.com/vaheedsk36/guess-my-number",
+  },
+  {
+    type: "WEB-APP",
+    title: "Notes App",
+    image: "react-notes-app",
+    link: "https://vaheedsk36.github.io/react-notes-app/",
+    source: "https://github.com/vaheedsk36/react-notes-app",
+  },
+  {
+    type: "WEB-APP",
+    title: "Dice Multiplayer",
+    image: "dice-multiplayer",
+    link: "https://vaheedsk36.github.io/dice-multiplayer",
+    source: "https://github.com/vaheedsk36/dice-multiplayer",
+  },
+  {
+    type: "PROJECT",
+    title: "Snake Game",
+    image: "snake-game",
+    link: "https://vaheedsk36.github.io/Snake-game",
+    source: "",
+  },
+  {
+    type: "PROJECT",
+    title: "Delta Assistant",
+    image: "delta-assistant",
+    link: "",
+    source: "https://github.com/vaheedsk36/Delta-Desktop-Virtual-Assistant",
+  },
+  {
+    type: "WEB-APP",
+    title: "Text Utility App",
+    image: "text-utility-app",
+    link: "https://vaheedsk36.github.io/text-utility-app/",
+    source: "https://github.com/vaheedsk36/text-utility-app",
+  },
+];
+
+
+export const socialMediaLinks = [
+  {
+    href: "https://github.com/vikass01",
+    icon: FaGithub,
+    backgroundColor: "gray.700",
+    hoverColor: "gray.600",
+  },
+  {
+    href: "https://www.instagram.com/",
+    icon: FaInstagram,
+    backgroundColor: "social.instagram",
+    hoverColor: "social.instagramHover",
+  },
+  {
+    href: "https://www.linkedin.com/",
+    icon: FaLinkedin,
+    backgroundColor: "social.linkedin",
+    hoverColor: "social.linkedinHover",
+  },
+  {
+    href: "https://twitter.com/",
+    icon: FaTwitter,
+    backgroundColor: "social.twitter",
+    hoverColor: "social.twitterHover",
+  },
+  {
+    href: "",
+    icon: FaHashnode,
+    backgroundColor: "social.hashnode",
+    hoverColor: "social.hashnodeHover",
+  },
+];
+
+export const query = `
+query Publication($after: String) {
+  publication(host: "codersk36.hashnode.dev") {
+    posts(first: 20, after: $after) {
+      totalDocuments
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          title
+          brief
+          slug
+          publishedAt
+          coverImage {
+            url
+          }
+        }
+      }
+    }
+  }
+}
+
+`;
