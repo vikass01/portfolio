d314cba8f1f843111df555893a11be5060837633
Vikas Panchal <eng.vikas90@gmail.com>
2024-02-26 05:03:40 +0530

    FG
    DFGDF

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -38,7 +38,7 @@ const Resume = () => {
               style={{
                 position: "absolute",
                 width: "100%",
-                height: "100%",
+                height: "200%",
                 top: 0,
                 left: 0,
                 border: "none",
